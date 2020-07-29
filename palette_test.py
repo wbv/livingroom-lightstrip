acf075aeb3808ce0cc9f210f03e5d3fefb169bc3
jean <kaliregenold@yahoo.com>
2020-07-29 13:23:06 -0600

    Make function from test code.

--- palette_test.py
+++ palette_test.py
@@ -8,17 +8,21 @@ import matplotlib.pyplot as plt
 from matplotlib.patches import Rectangle as rect
 import numpy as np
 
-r = requests.post('http://colormind.io/api/', data='{"model":"default"}')
-color_list = r.json()['result']
 
-fig = plt.figure()
-ax = fig.add_subplot(111)
+def plot_colors(color_list):
+    fig = plt.figure()
+    ax = fig.add_subplot(111)
 
-for i in range (0,5):
-    color = tuple([x/255 for x in color_list[i]])
-    rectangle = rect((i,0),1,5,color=color)
-    ax.add_patch(rectangle)
+    for i in range (0,5):
+        color = tuple([x/255 for x in color_list[i]])
+        rectangle = rect((i,0),1,5,color=color)
+        ax.add_patch(rectangle)
 
-plt.xlim([0,5])
-plt.ylim([0,5])
-plt.show()
+    plt.xlim([0,5])
+    plt.ylim([0,5])
+    plt.show()
+
+if __name__ == "__main__":
+    r = requests.post('http://colormind.io/api/', data='{"model":"default"}')
+    color_list = r.json()['result']
+    plot_colors(color_list)
