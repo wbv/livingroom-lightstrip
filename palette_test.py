173d8b867ab92e65a135bfadfc2f9ae3bc77bc2b
kaliRegenold <kaliregenold@yahoo.com>
2020-08-27 19:12:41 -0600

    Plot variable length palette.

--- palette_test.py
+++ palette_test.py
@@ -12,16 +12,17 @@ import numpy as np
 
 
 def plot_colors(color_list):
+    list_sz = len(color_list)
     fig = plt.figure()
     ax = fig.add_subplot(111)
 
-    for i in range (0,5):
+    for i in range (0, list_sz):
         color = tuple([x/255 for x in color_list[i]])
-        rectangle = rect((i,0),1,5,color=color)
+        rectangle = rect((i,0),1,list_sz,color=color)
         ax.add_patch(rectangle)
 
-    plt.xlim([0,5])
-    plt.ylim([0,5])
+    plt.xlim([0,list_sz])
+    plt.ylim([0,list_sz])
     # plt.show()
     plt.savefig('palette.png', bbox_inches='tight')
 
